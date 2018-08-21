eb92a1a20f65ad031671c10ded3ab059dba50de0
ᜋᜃ᜔ᜐ᜔ ᜉᜒᜇᜒᜈ᜔  ᜵ Maks Perin－✌ <perinmaks@outlook.ph>
2018-08-21 21:02:26 +0800

    Add files via upload

---
+++ ProjectFood/ProjectFood/app/build/generated/source/buildConfig/debug/com/example/acer/projectfood/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.acer.projectfood;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.acer.projectfood";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
